a8d8d7068b88cac71e4406e96038f4f4f7946507
ali raouf <chatiraqe@gmail.com>
2026-04-29 21:09:07 +0300

    Initialize Flask base_app template

---
+++ flask/base_app.py
@@ -0,0 +1,11 @@
+# Flask base template for Forge AI
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Forge AI Running"
+
+if __name__ == "__main__":
+    app.run(debug=True)
